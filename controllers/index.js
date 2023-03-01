9af38a369e3b85529a7e2fa5b331163de63315f5
Jessyjdi <116257968+Jessyjdi@users.noreply.github.com>
2023-02-28 23:13:57 -0800

    initial files

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const home = require('./publicCtr');
+const router = require('express').Router();
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require('./dashboard-routes.js');
+
+
+// router.use('/api', apiRoutes);
+
+// router.use('/', homeRoutes);
+
+// router.use('/dashboard', dashboardRoutes);
+
+module.exports = home;
+// module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrpyt = require("bcrypt");
+
+class User extends Model {
+    checkPw(loginPw) {
+        return bcrpyt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            types: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [5] },
+        },
+        trainer: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrpyt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrpyt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+    },
+    {
+        
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+        sequelize,
+    }
+);
+
+module.exports = User;
